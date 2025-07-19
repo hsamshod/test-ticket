90c0e8504ee393c59c85da2c2788cd6400afb7fa
Shamshod Zhamolov <shamshod.zhamolov@yandex.ru>
2025-07-19 06:56:36 +0500

    -  add relations, fix bugs

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -1,11 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Collection;
 
+/**
+ * Class Ticket
+ *
+ * @property-read Category $category
+ * @property-read TicketMessage[]|Collection $messages
+ */
 class Ticket extends Model
 {
     protected $guarded = [];
@@ -15,7 +24,7 @@ class Ticket extends Model
         return $this->hasMany(TicketMessage::class);
     }
 
-    public function cateogry(): BelongsTo
+    public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
     }
